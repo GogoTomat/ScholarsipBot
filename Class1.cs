854438c1d0e8842d22203657d8924e6cd4c1ed78
Mark Komissarov <88964256+GogoTomat@users.noreply.github.com>
2022-02-25 16:14:17 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScholarshipBot
+{
+    static class Info
+    {
+        public static int money = 100;
+        public static int b1 = 0;
+        public static int n1 = 0;
+        public static int b2 = 0;
+        public static int n2 = 0;
+        public static int b3 = 0;
+        public static int n3 = 0;
+        public static int b4 = 0;
+        public static int n4 = 0;
+        public static int b5 = 0;
+        public static int n5 = 0;
+        public static int acc = 1 + (b1 * n1 + b2 * n2 + b3 * n3 + b4 * n4 + b5 * n5);
+    };
+}
