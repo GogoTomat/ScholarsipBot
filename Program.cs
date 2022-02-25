e093d027f0fefcd7bdae2f1a43cebe10e0a1b913
Mark Komissarov <88964256+GogoTomat@users.noreply.github.com>
2022-02-25 10:14:18 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,17 +21,7 @@ botClient.StartReceiving(
 
 var me = await botClient.GetMeAsync();
 
-double money = 0;
-double booster1 = 0;
-double Bn1 = 0;
-double booster2 = 0;
-double Bn2 = 0;
-double booster3 = 0;
-double Bn3 = 0;
-double booster4 = 0;
-double Bn4 = 0;
-double booster5 = 0;
-double Bn5 = 0;
+
 
 Console.WriteLine($"Start listening for @{me.Username}");
 Console.ReadLine();
@@ -41,12 +31,104 @@ cts.Cancel();
 //вот эта штука отвечает за отслеживания нажажатия кнопки 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
+    double money = 10;
+
+    double booster1 = 0;
+    double Bn1 = 0;
+    double booster2 = 0;
+    double Bn2 = 0;
+    double booster3 = 0;
+    double Bn3 = 0;
+    double booster4 = 0;
+    double Bn4 = 0;
+    double booster5 = 1;
+    double Bn5 = 1;
+
     if (update.Type == UpdateType.CallbackQuery)
     {
-        //там ниже кнопка, у нее CallBackData = farm, значит мы по колбек дате отслеживаем нажатие именно этой кнопки
+        //там ниже кнопка, у нее CallBackData = farm, значит мы по колбек дате отслеживаем нажатие именно этой кнопки, не работает изменение переменной
         if (update.CallbackQuery.Data == "farm")
         {
-            await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "+points");
+            money += 0.01 + (booster1 * Bn1 + booster2 * Bn2 + booster3 * Bn3 + booster4 * Bn4 + booster5 * Bn5);
+            await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "1");
+            Console.WriteLine(money);
+        }
+        //отслеживание кнопок магазина, толком не работает изменение переменных
+        if (update.CallbackQuery.Data == "shaurma")
+        {
+            if (money >= 10)
+            {
+                booster1 += 0.01;
+                Bn1 += 1;
+                money -= 10;
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Куплена 1 шаурма!");
+                //Console.WriteLine(booster1);
+                //Console.WriteLine(Bn1);
+                //Console.WriteLine(money);
+            }
+            else {
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Недостаточно денег");
+            }
+        }
+       
+        if (update.CallbackQuery.Data == "Nurb")
+        {
+            if (money >= 50)
+            {
+                booster2 += 0.05;
+                Bn2 += 1;
+                money -= 50;
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Куплен 1 энергетик!");
+            }
+            else
+            {
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Недостаточно денег");
+            }
+        }
+
+        if (update.CallbackQuery.Data == "cons")
+        {
+            if (money >= 100)
+            {
+                booster3 += 0.5;
+                Bn3 += 1;
+                money -= 100;
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Вы успешно сходили на консультацию!");
+            }
+            else
+            {
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Недостаточно денег");
+            }
+        }
+
+        if (update.CallbackQuery.Data == "lec")
+        {
+            if (money >= 500)
+            {
+                booster4 += 1;
+                Bn4 += 1;
+                money -= 500;
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Вы успешно сходили на лекцию!");
+            }
+            else
+            {
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Недостаточно денег");
+            }
+        }
+
+        if (update.CallbackQuery.Data == "ex")
+        {
+            if (money >= 1000)
+            {
+                booster5 += 5;
+                Bn5 += 1;
+                money -= 1000;
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Вы успешно сдали экзамен!");
+            }
+            else
+            {
+                await botClient.AnswerCallbackQueryAsync(callbackQueryId: update.CallbackQuery.Id, text: "Недостаточно денег");
+            }
         }
         return;
     }
@@ -68,6 +150,20 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
         },
 
     });
+
+    InlineKeyboardMarkup shop = new(new[]
+{
+        new []
+        {
+            InlineKeyboardButton.WithCallbackData(text: "1", callbackData: "shaurma"),
+            InlineKeyboardButton.WithCallbackData(text: "2", callbackData: "Nurb"),
+            InlineKeyboardButton.WithCallbackData(text: "3", callbackData: "cons"),
+            InlineKeyboardButton.WithCallbackData(text: "4", callbackData: "lec"),
+            InlineKeyboardButton.WithCallbackData(text: "5", callbackData: "ex"),
+        },
+
+    });
+
 
     //начало
     if (messageText == "/start")
@@ -93,10 +189,10 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     {
         Message sentNo = await botClient.SendTextMessageAsync(
                 chatId: chatId, text: "Мне нечего кушать, ты должен быть готов.", cancellationToken: cancellationToken);
-    }   
+    }
 
-        //обработка магазина, надо дописать кнопки 
-        else if(messageText == "/shop")
+        //обработка магазина
+        else if (messageText == "/shop")
     {
         Message sentShop = await botClient.SendTextMessageAsync(
             chatId: chatId,
@@ -105,6 +201,7 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             "3. Сходить на консультацию за 100 очков(+0.5 за клик)" + '\n' +
            " 4. Сходить на лекция за 500 очков(+1 за клик) " + '\n' +
             "5. Сходить на экзамен за 1000 очков(+5 за клик)",
+            replyMarkup: shop,
             cancellationToken: cancellationToken
             );
     }
